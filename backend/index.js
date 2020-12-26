d39bb784a2a24190cb6e1b42b68ae1ccf2e7b2ff
Micaelo <prodomitor@gmail.com>
2020-12-26 20:37:59 +0100

    mysql configured

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+})
+
+db.connect((err) => {
+  if(err) {
+    throw err;
+  }
+  console.log('MySql connected')
+});
+
 const app = express();
 const port = 3000;
 
@@ -11,6 +25,16 @@ app.get('/', (req, res) => {
 app.get('/welcome', function (req, res) {
   res.send('Hello World 2!')
 });
+
+app.get('/createdb', (req, res) => {
+  const sql = 'create database d_po';
+  db.query(sql, (err, result) => {
+    if(err) {
+      throw err;
+    }
+   res.json({'message': result})
+  })
+})
 
 app.listen(port, ()=> {
   console.log(`This app is listenning at ${port}`);
